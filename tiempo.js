f297116c0f20d44cc8cc32eb7e6e0f23088c9ef4
Karla Sierra <ksierra@ucol.mx>
2020-03-24 21:52:22 -0600

    Tiempo terminado

---
+++ tiempo.js
@@ -0,0 +1,16 @@
+export default class Tiempo {
+  constructor(hora, minutos, periodo) {
+      this._hora = hora
+      this._minutos = minutos
+      this._periodo = periodo
+  }
+  getFormato12(){
+    return `${this._hora}:${this._minutos} ${this._periodo}`
+  }
+  getFormato24(){
+    if (this._periodo.toUpperCase() === "PM" && this._hora != 12) {
+      this._hora = this._hora + 12
+    }
+    return `${this._hora}:${this._minutos}`
+  }
+}
